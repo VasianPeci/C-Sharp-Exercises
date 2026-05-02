d636e365ef1db588c6cc7ef9c68fbcd62417af44
VasianPeci <vasianpeci26@gmail.com>
2026-05-02 11:01:34 +0200

    Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check if it is palindrome or not:");
+
+            string str = Console.ReadLine();
+
+            string palindrome = IsPalindrome(str) ? "is" : "is not";
+
+            Console.WriteLine($"The string you entered ({str}) {palindrome} a palindrome!");
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            for (int i = 0; i < str.Length/2; i++)
+            {
+                if (str[i] != str[str.Length-1-i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
